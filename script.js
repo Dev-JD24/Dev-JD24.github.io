b0916e24d8918912b298e462cde9fd4345136b30
Jaydeep Zala <122034381+Dev-JD24@users.noreply.github.com>
2025-09-21 17:29:00 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("toggleParallax").addEventListener("click", function () {
-  document.body.classList.toggle("no-parallax");
-  this.textContent = document.body.classList.contains("no-parallax")
-    ? "Enable Parallax"
-    : "Disable Parallax";
-});
